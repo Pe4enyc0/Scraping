50009204010f7a6e9aa7381a0e137986cc54272e
Pe4enyc0 <141564773+Pe4enyc0@users.noreply.github.com>
2023-10-08 17:18:31 +0300

    - added searching authors tags, and table of they

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import sqlite3
 
 url = 'https://quotes.toscrape.com/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 quotes = soup.find_all('span', class_='text')
 authors = soup.find_all('small', class_='author')
+tags = soup.find_all('div', class_='tags')
+
 some_data = dict()
 for i in range(len(quotes)):
-    some_data[authors[i].text] = quotes[i].text
+    print(quotes[i].text)
+    print(authors[i].text)
+    print(tags[i].text.split()[1:])
+    print('_______________________')
 
 print(some_data)
+
 with open('result.json', 'w', encoding='utf-8') as file:
     json.dump(some_data,file)
+
+createSQL = """CREATE TABLE quotes (author TEXT, quote TEXT, tags TEXT)"""
+conn = sqlite3.connect('data_from_web.db')
+cursor = conn.cursor()
+cursor.execute(createSQL)
+SQL = """INSERT INTO quotes(author, quote, tags) VALUES (?,?,?)"""
+
+for i in range(len(quotes)):
+    quote = quotes[i].text
+    author = authors[i].text
+    tag = ", ".join(tags[i].text.split()[1:])
+    cursor.execute(SQL, [author, quote, tag])
+    conn.commit()
+
 
